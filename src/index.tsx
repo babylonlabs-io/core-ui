b6471c949f8bbb942e278cff530378f358aa3ae2
David Totraev <totraev@gmail.com>
2024-11-11 14:35:39 +0500

    init

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+export * from "./components/Text";
+export * from "./components/Heading";
+export * from "./components/Button";
+export * from "./components/Avatar";
+export * from "./components/Input";
+export * from "./components/Dialog";
+export * from "./components/Chip";
+export * from "./components/Portal";
+
+export { ScrollLocker } from "@/context/Dialog.context";
