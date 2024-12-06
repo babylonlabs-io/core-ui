513b97d4e3ee160b40009b1f629e21d1b2eb9657
David Totraev <totraev@gmail.com>
2024-12-06 19:25:03 +0500

    fix: loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -10,7 +10,7 @@ export function Loader({ size = 40, className }: LoaderProps) {
   return (
     <span className={twMerge("bbn-loader text-current", className)} style={{ width: size, height: size }}>
       <svg viewBox="22 22 44 44">
-        <circle cx="44" cy="44" r="20.2" fill="none" stroke="currentColor" stroke-width="3.6" />
+        <circle cx="44" cy="44" r="20.2" fill="none" stroke="currentColor" strokeWidth="3.6" />
       </svg>
     </span>
   );
