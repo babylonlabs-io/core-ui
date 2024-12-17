c5fafaa50308d5cfd90dbd13c2afffe930e22136
jeremy-babylonlabs <jeremy.t@babylonlabs.io>
2024-12-17 14:44:43 +0700

    table improvement

---
+++ .changeset/cyan-clouds-play.md
@@ -0,0 +1,5 @@
+---
+"@babylonlabs-io/bbn-core-ui": patch
+---
+
+table improvement
--- src/components/Table/types/index.ts
+++ src/components/Table/types/index.ts
@@ -14,5 +14,6 @@ export interface TableProps<T extends { id: string | number }> {
   hasMore?: boolean;
   loading?: boolean;
   onLoadMore?: () => void;
-  onRowSelect?: (row: T) => void;
+  onRowSelect?: (row: T | null) => void;
+  isRowSelectable?: (row: T) => boolean;
 }
