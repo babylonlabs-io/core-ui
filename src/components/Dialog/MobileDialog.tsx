fe3eba5bfe5dde5db417e28f4858df9fb29c5935
Tong Li <tongli0809@gmail.com>
2025-01-27 19:33:18 +1100

    The following change has been made to MobileDialog: * MobileDialog will now always take up 80% vh. * Added a drag bar at the top of the panel which the user can drag to close. * The MobileDialog itself is now scrollable.

--- src/components/Dialog/MobileDialog.tsx
+++ src/components/Dialog/MobileDialog.tsx
@@ -1,5 +1,6 @@
-import { type DetailedHTMLProps, type HTMLAttributes } from "react";
+import { useEffect, type DetailedHTMLProps, type HTMLAttributes } from "react";
 import { twJoin } from "tailwind-merge";
+import { motion, useAnimate, useDragControls, useMotionValue } from "motion/react";
 
 import { Portal } from "@/components/Portal";
 import { useModalManager } from "@/hooks/useModalManager";
@@ -10,24 +11,80 @@ export interface MobileDialogProps extends DetailedHTMLProps<HTMLAttributes<HTML
   onClose?: () => void;
 }
 
-export const MobileDialog = ({ children, open = false, className, onClose, ...restProps }: MobileDialogProps) => {
+export const MobileDialog = ({ children, open = false, className, onClose }: MobileDialogProps) => {
   const { mounted, unmount } = useModalManager({ open });
+  const [scope, animate] = useAnimate();
+  const dragControls = useDragControls();
+  const y = useMotionValue(0);
+
+  const handleClose = () => {
+    // This is getting around the issue that the y starts with 0% instead of a number
+    const yStart = typeof y.get() === "number" ? y.get() : 0;
+    /**
+     * Since the backdrop animation isn't controlled manually, we intentially
+     * NOT await on the animate here to make sure the backdrop fadeout at the
+     * same time as the dialog.
+     */
+    animate(scope.current, {
+      y: [yStart, 1000],
+    });
+    if (onClose) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    /**
+     * This is to cover the case where the close was triggered
+     * other than from the drag bar
+     */
+    if (scope.current && !open) {
+      handleClose();
+    }
+  }, [open]);
 
   return (
     <Portal mounted={mounted}>
-      <div
-        {...restProps}
-        className={twJoin(
-          "bbn-dialog-mobile",
-          open ? "animate-mobile-modal-in" : "animate-mobile-modal-out",
-          className,
-        )}
-        onAnimationEnd={unmount}
+      <motion.div
+        ref={scope}
+        initial={{ y: "100%" }}
+        animate={{ y: "0%" }}
+        style={{
+          y,
+        }}
+        transition={{
+          ease: "easeInOut",
+        }}
+        onDragEnd={() => {
+          if (y.get() >= 100) {
+            handleClose();
+          }
+        }}
+        drag="y"
+        dragControls={dragControls}
+        dragListener={false}
+        dragConstraints={{
+          top: 0,
+          bottom: 0,
+        }}
+        dragElastic={{
+          top: 0,
+          bottom: 0.5,
+        }}
+        className={twJoin("bbn-dialog-mobile", className)}
       >
-        {children}
-      </div>
+        <div className="absolute inset-x-0 top-0 z-10 flex justify-center p-4">
+          <button
+            className="h-1 w-12 cursor-grab touch-none rounded-full bg-secondary-strokeLight active:cursor-grabbing"
+            onPointerDown={(e) => {
+              dragControls.start(e);
+            }}
+          />
+        </div>
+        <div>{children}</div>
+      </motion.div>
 
-      <Backdrop open={open} onClick={onClose} />
+      <Backdrop open={open} onClick={handleClose} onAnimationEnd={unmount} />
     </Portal>
   );
 };
