5aee6b01c656c46e9085e26254fc80477163327e
Jony Bursztyn <jonathan@bursztyn.io>
2025-07-08 10:55:16 +0200

    feat: phase 3 design changes (#132)
    * Phase 3 design changes
    
    * Make design backwards compatible
    
    * Refactor
    
    * Fix full width listitem

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,11 +1,12 @@
 import { type PropsWithChildren, Children, cloneElement, isValidElement, ReactElement } from "react";
-import { twMerge } from "tailwind-merge";
 import "./List.css";
+import { twMerge } from "tailwind-merge";
 
 import { type ListItemProps, ListItem } from "./components/ListItem";
 
 export interface ListProps {
   className?: string;
+  newDesign?: boolean;
   orientation: "adaptive" | "horizontal" | "vertical";
   children: ReactElement<ListItemProps, typeof ListItem> | ReactElement<ListItemProps, typeof ListItem>[];
 }
@@ -16,11 +17,28 @@ const ROW_ORIENTATION = {
   vertical: "horizontal",
 } as const;
 
-export function List({ className, orientation = "vertical", children }: PropsWithChildren<ListProps>) {
+export function List({
+  newDesign = false,
+  className,
+  orientation = "vertical",
+  children,
+}: PropsWithChildren<ListProps>) {
+  const getListClasses = () => {
+    if (newDesign) {
+      return twMerge("bbn-list-new-design", `bbn-list-new-design-${orientation}`, className);
+    }
+    return twMerge("bbn-list", `bbn-list-${orientation}`, className);
+  };
+
   return (
-    <div className={twMerge("bbn-list", `bbn-list-${orientation}`, className)}>
+    <div className={getListClasses()}>
       {Children.map(children, (item) =>
-        isValidElement(item) ? cloneElement(item, { orientation: ROW_ORIENTATION[orientation] }) : item,
+        isValidElement(item)
+          ? cloneElement(item, {
+              orientation: ROW_ORIENTATION[orientation],
+              newDesign,
+            })
+          : item,
       )}
     </div>
   );
